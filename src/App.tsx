49238d97cfe69d42a26ba1ddd565f26198877f0d
Lucas <lucasfraza0@outlook.com>
2023-07-16 03:03:24 -0300

    initial config router and config defaul theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Router } from 'Router'
+import { BrowserRouter } from 'react-router-dom'
+
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from 'styles/global'
+import { defaultTheme } from 'styles/themes/default'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
